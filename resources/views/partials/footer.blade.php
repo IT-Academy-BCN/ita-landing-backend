498cdf0bc711f4e7fe440bcc3e522b00240242da
Joël Arana <joel.aranag@gmail.com>
2023-04-20 14:44:45 +0200

    Added footer template with link to logo. (#14)

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="bg-[#BA007C]">
+    <div class="mx-auto w-full py-6 text-white text-sm">
+      <div>
+        <div class="flex justify-center items-center">
+          <img
+            src="../../../public/images/ITA_footer_logo.png"
+            class="h-10"
+            alt="IT Academy Footer Logo"
+          />
+        </div>
+        <div class="flex justify-center mt-10 font-semibold">
+          <a class="mx-5" href="">Barcelona Activa</a>
+          <a class="mx-5" href="">IT Academy</a>
+          <a class="mx-5" href="">¿Cómo colaborar?</a>
+        </div>
+        <div class="flex justify-center mt-20 font-medium pb-3">
+          <a class="mx-7" href="">Términos</a>
+          <a class="mx-7" href="">Privacidad</a>
+          <a class="mx-7" href="">Cookies</a>
+        </div>
+      </div>
+    </div>
+</footer>
