97a685588396a9151ba97795dbf2dd8a0d72765a
Jordi Julià <jordijs6@gmail.com>
2023-04-20 13:05:04 +0200

    Add text contents and html structure

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,7 +10,93 @@
 <body>
     <h1 class="text-3xl font-bold underline">
         Hello world!
-        
-      </h1>
+    </h1>
+
+    <div class="container mx-auto">
+    <section class="flex flex-col">
+        <h2>
+            Directorio de aplicaciones IT Academy
+        </h2>
+        <div class="flex flex-row">
+            <div>
+                Terminadas
+            </div>
+            <div>
+                En curso
+            </div>
+            <div>
+                Próximas
+            </div>
+        </div>
+        <div class="flex flex-col">
+            <div class="flex flex-col">
+                <h3>
+                    Wiki
+                </h3>
+                <p>
+                    Plataforma en la que l@s alumn@s pueden subir y votar recursos que encuentran útiles para su aprendizaje. 
+                </p>
+                <button>
+                    Ir a app
+                </button>
+            </div>
+            <div class="flex flex-col">
+                <h3>
+                    Challenges & Hackathons
+                </h3>
+                <p>
+                    Lanzamiento de retos periódicos y  Hackathons. El desempeño de l@s alumn@s pueden ser consumidos por otras apps
+                </p>
+                <button>
+                    Ir a app
+                </button>
+            </div>
+            <div class="flex flex-col">
+                <h3>
+                    Game!
+                </h3>
+                <p>
+                    L@s alumn@s ganan puntos al realizar actividades de ayuda a sus compañeros. Hay un ranking, perfil con premios ¡y mucho más!
+                </p>
+                <button>
+                    Ir a app
+                </button>
+            </div>
+            <div class="flex flex-col">
+                <h3>
+                    ITA Profiles
+                </h3>
+                <p>
+                    Directorio de perfiles de la IT Academy, con información relevante como desempeño en retos y hackathons.
+                </p>
+                <button>
+                    Ir a app
+                </button>
+            </div>
+            <div class="flex flex-col">
+                <h3>
+                    Reports
+                </h3>
+                <p>
+                    Dashboard en la que l@s estudiantes, mentor@s y staff pueden encontrar información actualizada sobre la academia.
+                </p>
+                <button>
+                    Ir a app
+                </button>
+            </div>
+            <div class="flex flex-col">
+                <h3>
+                    ITA Brain
+                </h3>
+                <p>
+                    Predicción de probabilidad de abandono de un alumno en función de la información recogida en las apps y moodle.
+                </p>
+                <button>
+                    Ir a app
+                </button>
+            </div>
+        </div>
+    </section>
+    </div>
 </body>
 </html>
