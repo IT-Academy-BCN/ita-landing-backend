e524bcb249eb466ad403af20cbcb2d90939f0817
FortGH <fortjordi15@gmail.com>
2023-04-24 20:16:02 +0200

    frequently asked questions

---
+++ resources/views/components/faqs.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang=en>
+<head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Landing</title>
+
+        <!-- Styles -->
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+</head>
+<body>
+    <section>
+        <h1 class="text-center text-2xl"><strong>Preguntas Frecuentes</strong></h1>
+          <ul class="grid my-10 ml-32 ">
+              <li class="my-2">
+                <div class="rounded group inline-block shadow-xl py-6 w-3/4 ">
+                    <button class="ml-2  shadow-2xl"><strong>¿Tengo que acabar especializacion para colaborar en proyectos?</strong></button>
+                    <div class=" hidden text-gray-700 pt-1 group-hover:block" >
+                    <div class="rounded bg-gray-200 hover: py-2 px-4 mx-4 block flex-1 ">1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur illum possimus repellendus blanditiis
+                    vel minus magni laudantium quam delectus, doloremque, officia vero mollitia perspiciatis. Ipsum eos fugiat nemo ut.
+                    </div>
+                    </div>
+                </div>
+              </li>
+              <li class="my-2">
+                <div class="rounded group inline-block shadow-xl py-6 w-3/4">
+                  <button  class="ml-2  shadow-2xl"><strong>¿Cuantos son los tiempos minimos de estancia en proyecto?</strong></button>
+                  <div class=" hidden text-gray-700 pt-1 group-hover:block" >
+                    <div class="rounded bg-gray-200 hover: py-2 px-4 mx-4block flex-1">2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur illum possimus repellendus blanditiis
+                  vel minus magni laudantium quam delectus, doloremque, officia vero mollitia perspiciatis. Ipsum eos fugiat nemo 
+                  </div>
+                </div>
+              </div>
+              </li>
+              <li class="my-2">
+                <div class="rounded group inline-block shadow-xl py-6 w-3/4">
+                  <button  class="ml-2  shadow-2xl"><strong>¿Puedo poner el Linkedin o CV proyectos como experiencia laboral?</strong></button>
+                  <div class=" hidden text-gray-700 pt-1 group-hover:block" >
+                    <div class="rounded bg-gray-200 hover: py-2 px-4 mx-4 block flex-1">3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur illum possimus repellendus blanditiis
+                  vel minus magni laudantium quam delectus, doloremque, officia vero mollitia perspiciatis. Ipsum eos fugiat nemo ut.
+                  </div>
+                </div>
+              </div>
+              </li>
+              <li class="my-2">
+                <div class="rounded group inline-block shadow-xl py-6 w-3/4">
+                  <button  class="ml-2  shadow-2xl"><strong>¿Puedo estar trabajando mientras colaboro en proyectos?</strong></button>
+                  <div class=" hidden text-gray-700 pt-1 group-hover:block" >
+                    <div class="rounded bg-gray-200 hover: py-2 px-4 mx-4 block flex-1">4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur illum possimus repellendus blanditiis
+                  vel minus magni laudantium quam delectus, doloremque, officia vero mollitia perspiciatis. Ipsum eos fugiat nemo ut.
+                  </div>
+                </div>
+              </div>
+              </li>
+              <li class="my-2">
+                <div class="rounded group inline-block shadow-xl py-6 w-3/4">
+                  <button  class="ml-2  shadow-2xl"><strong>Creo que mi nivel no es suficiente ¿aun asi puedo colaborar en proyectos?</strong></button>
+                  <div class=" hidden text-gray-700 pt-1 group-hover:block" >
+                    <div class="rounded bg-gray-200 hover:py-2 px-4 mx-4 block flex-1">5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur illum possimus repellendus blanditiis
+                  vel minus magni laudantium quam delectus, doloremque, officia vero mollitia perspiciatis. Ipsum eos fugiat nemo ut.
+                  </div>
+                </div>
+              </div>
+              </li>
+          </ul>
+    </section>
+</body>
