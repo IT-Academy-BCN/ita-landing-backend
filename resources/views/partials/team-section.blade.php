ee05f4f04e0f1edee343fbdb9c14c7bdf4894b96
Lauddar <lauragarciarod@gmail.com>
2023-04-25 21:17:45 +0200

    add hover effect to cards

--- resources/views/partials/team-section.blade.php
+++ resources/views/partials/team-section.blade.php
@@ -10,11 +10,9 @@
     <!-- Card React&Node -->
     <div class="relative mt-4">
       <svg width="360" height="410" viewBox="0 0 268 329" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z"
-          fill="#F0F0F0" />
+        <path d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z" fill="#F0F0F0" />
       </svg>
-      <div class="overlay absolute top-0 left-0 flex flex-col w-full h-full justify-between">
+      <div class="overlay absolute inset-0 flex flex-col w-full h-full justify-between">
         <div class="p-6 pb-0 1/5">
           <p class="font-normal font-medium text-base leading-5 text-gray-600 font-poppins">/ 01</p>
         </div>
@@ -25,17 +23,28 @@
         <div class="flex justify-center items-center p-3 h-1/5">
           <p class="text-center font-poppins font-medium text-2xl text-black">React & Node</p>
         </div>
+        <a href="#" class="overlay absolute inset-0 flex flex-col m-[45px] mt-[75px] h-4/5 overflow-hidden opacity-0 hover:opacity-100" style="background-color: #F0F0F0;">
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden mb-2">Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.</p>
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden">Velitbus vulputate neque lacinia faucibus blandit diam parturient luctus.</p>
+          <div class="flex items-center mt-5">
+            <img class="absolute mr-[18px]" src="{{ asset('img/Ellipse.png') }}" alt="">
+            <img class="absolute  ml-2 mr-[18px]" src="{{ asset('img/Vector.png') }}" alt="">
+            <div class="ml-8 font-black">
+              <p>
+                Ver más
+              </p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
 
     <!-- Card Angular&Java-->
     <div class="relative">
       <svg width="360" height="410" viewBox="0 0 268 329" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z"
-          fill="#F0F0F0" />
+        <path d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z" fill="#F0F0F0" />
       </svg>
-      <div class="overlay absolute top-0 left-0 flex flex-col w-full h-full justify-between">
+      <div class="overlay absolute inset-0 flex flex-col w-full h-full justify-between">
         <div class="p-6 pb-0 1/5">
           <p class="font-normal font-medium text-base leading-5 text-gray-600 font-poppins">/ 02</p>
         </div>
@@ -46,17 +55,28 @@
         <div class="flex justify-center items-center p-3 h-1/5">
           <p class="text-center font-poppins font-medium text-2xl text-black">Angular & Java</p>
         </div>
+        <a href="#" class="overlay absolute inset-0 flex flex-col m-[45px] mt-[75px] h-4/5 overflow-hidden opacity-0 hover:opacity-100" style="background-color: #F0F0F0;">
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden mb-2">Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.</p>
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden">Velitbus vulputate neque lacinia faucibus blandit diam parturient luctus.</p>
+          <div class="flex items-center mt-5">
+            <img class="absolute mr-[18px]" src="{{ asset('img/Ellipse.png') }}" alt="">
+            <img class="absolute  ml-2 mr-[18px]" src="{{ asset('img/Vector.png') }}" alt="">
+            <div class="ml-8 font-black">
+              <p>
+                Ver más
+              </p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
 
     <!-- Card PHP-->
     <div class="relative">
       <svg width="360" height="410" viewBox="0 0 268 329" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z"
-          fill="#F0F0F0" />
+        <path d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z" fill="#F0F0F0" />
       </svg>
-      <div class="overlay absolute top-0 left-0 flex flex-col w-full h-full justify-between">
+      <div class="overlay absolute inset-0 flex flex-col w-full h-full justify-between">
         <div class="p-6 pb-0 1/5">
           <p class="font-normal font-medium text-base leading-5 text-gray-600 font-poppins">/ 03</p>
         </div>
@@ -66,17 +86,28 @@
         <div class="flex justify-center items-center p-3 h-1/5">
           <p class="text-center font-poppins font-medium text-2xl text-black">PHP</p>
         </div>
+        <a href="#" class="overlay absolute inset-0 flex flex-col m-[45px] mt-[75px] h-4/5 overflow-hidden opacity-0 hover:opacity-100" style="background-color: #F0F0F0;">
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden mb-2">Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.</p>
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden">Velitbus vulputate neque lacinia faucibus blandit diam parturient luctus.</p>
+          <div class="flex items-center mt-5">
+            <img class="absolute mr-[18px]" src="{{ asset('img/Ellipse.png') }}" alt="">
+            <img class="absolute  ml-2 mr-[18px]" src="{{ asset('img/Vector.png') }}" alt="">
+            <div class="ml-8 font-black">
+              <p>
+                Ver más
+              </p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
 
     <!-- Card Data Science -->
     <div class="relative">
       <svg width="360" height="410" viewBox="0 0 268 329" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z"
-          fill="#F0F0F0" />
+        <path d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z" fill="#F0F0F0" />
       </svg>
-      <div class="overlay absolute top-0 left-0 flex flex-col w-full h-full justify-between">
+      <div class="overlay absolute inset-0 flex flex-col w-full h-full justify-between">
         <div class="p-6 pb-0 1/5">
           <p class="font-normal font-medium text-base leading-5 text-gray-600 font-poppins">/ 04</p>
         </div>
@@ -86,6 +117,19 @@
         <div class="flex justify-center items-center p-3 h-1/5">
           <p class="text-center font-poppins font-medium text-2xl text-black">Data Science</p>
         </div>
+        <a href="#" class="overlay absolute inset-0 flex flex-col m-[45px] mt-[75px] h-4/5 overflow-hidden opacity-0 hover:opacity-100" style="background-color: #F0F0F0;">
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden mb-2">Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.</p>
+          <p class="font-poppins font-medium text-lg text-black overflow-hidden">Velitbus vulputate neque lacinia faucibus blandit diam parturient luctus.</p>
+          <div class="flex items-center mt-5">
+            <img class="absolute mr-[18px]" src="{{ asset('img/Ellipse.png') }}" alt="">
+            <img class="absolute  ml-2 mr-[18px]" src="{{ asset('img/Vector.png') }}" alt="">
+            <div class="ml-8 font-black">
+              <p>
+                Ver más
+              </p>
+            </div>
+          </div>
+        </a>
       </div>
     </div>
 
