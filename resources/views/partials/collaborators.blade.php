2b78ecbdfd34a9be6a31b003bf3a61ef70ca4e34
Joël Arana <joel.aranag@gmail.com>
2023-04-26 23:38:18 +0200

    Added collaborator card (#18)

--- resources/views/partials/collaborators.blade.php
+++ resources/views/partials/collaborators.blade.php
@@ -1,20 +1,28 @@
-<section>
+<section class="my-10">
     <div class="flex justify-center">
         <p class="text-4xl font-extrabold my-5">Colaboradores de proyectos</p>
     </div>
     <div class="flex justify-center my-5">
         <div class="flex justify-center w-[726px]">
             <div class="flex navbar bg-base-100 drop-shadow-md justify-between rounded-full">
-                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Angular</a>
-                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">React</a>
-                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">PHP</a>
-                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Node</a>
-                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Java</a>
-                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Data Science</a>
+                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Angular</a>
+                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">React</a>
+                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">PHP</a>
+                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Node</a>
+                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Java</a>
+                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Data Science</a>
             </div>
         </div>
     </div>
     <div class="flex justify-end my-5">
-        <a href="">Ver Todos ></a>
+        <a class="font-bold text-[#025E7E] active:underline " href="">Ver Todos ></a>
+    </div>
+    <div class="flex justify-center">
+        <div class="grid-rows-3">
+            <div class="flex justify-center">
+                <img class="mask mask-circle h-20 w-20" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                <p class="text-xl mt-6 ml-5">Joël Arana</p>
+            </div>
+        </div>
     </div>
 </section>
