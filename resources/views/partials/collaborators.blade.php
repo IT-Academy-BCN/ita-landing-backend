7b380d0eec69f39200532192e8f4f1a4068628dc
Joël Arana <joel.aranag@gmail.com>
2023-04-26 18:34:52 +0200

    Added title navbar and ver todos action (#18)

---
+++ resources/views/partials/collaborators.blade.php
@@ -0,0 +1,20 @@
+<section>
+    <div class="flex justify-center">
+        <p class="text-4xl font-extrabold my-5">Colaboradores de proyectos</p>
+    </div>
+    <div class="flex justify-center my-5">
+        <div class="flex justify-center w-[726px]">
+            <div class="flex navbar bg-base-100 drop-shadow-md justify-between rounded-full">
+                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Angular</a>
+                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">React</a>
+                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">PHP</a>
+                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Node</a>
+                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Java</a>
+                <a class="btn btn-ghost text-sm rounded-3xl active:bg-[#025E7E]">Data Science</a>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-end my-5">
+        <a href="">Ver Todos ></a>
+    </div>
+</section>
