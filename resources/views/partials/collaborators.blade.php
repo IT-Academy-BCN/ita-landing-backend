df2bf1c312ff769c654c941c1ca6f615eb03b309
Joël Arana <joel.aranag@gmail.com>
2023-05-01 19:42:27 +0200

    Added carousel (#18)

--- resources/views/partials/collaborators.blade.php
+++ resources/views/partials/collaborators.blade.php
@@ -5,75 +5,79 @@
     <div class="flex justify-center my-5">
         <div class="flex justify-center w-[726px]">
             <div class="flex navbar bg-base-100 drop-shadow-md justify-between rounded-full">
-                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Angular</a>
-                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">React</a>
-                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">PHP</a>
-                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Node</a>
-                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Java</a>
-                <a class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Data Science</a>
+                <a href="#angular" class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Angular</a>
+                <a href="#react" class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">React</a>
+                <a href="#php" class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">PHP</a>
+                <a href="#node" class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Node</a>
+                <a href="#java" class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Java</a>
+                <a href="#dscience" class="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]">Data
+                    Science</a>
             </div>
         </div>
     </div>
     <div class="flex justify-end my-5 mr-10">
         <a class="font-bold text-[#025E7E] active:underline " href="">Ver Todos ></a>
     </div>
-    <div class="flex justify-center">
-        <div class="grid grid-cols-3 gap-3 w-3/4">
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+    <div class="carousel h-96 w-full">
+        <div id="angular" class="carousel-item w-screen">
+            <div class="grid grid-cols-3 gap-3 w-3/4">
+                <div class="flex justify-center my-5">
+                    <div class="w-24 h-24 overflow-hidden rounded-full">
+                        <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                    </div>
+                    <p class="text-2xl mt-8 ml-5">Joël Arana</p>
                 </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
             </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+        </div>
+        <div id="react" class="carousel-item w-full">
+            <div class="grid grid-cols-3 gap-3 w-3/4">
+                <div class="flex justify-center my-5">
+                    <div class="w-24 h-24 overflow-hidden rounded-full">
+                        <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                    </div>
+                    <p class="text-2xl mt-8 ml-5">Joël Arana</p>
                 </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
-            </div>            
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
-                </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
             </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+        </div>
+        <div id="php" class="carousel-item w-full">
+            <div class="grid grid-cols-3 gap-3 w-3/4">
+                <div class="flex justify-center my-5">
+                    <div class="w-24 h-24 overflow-hidden rounded-full">
+                        <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                    </div>
+                    <p class="text-2xl mt-8 ml-5">Joël Arana</p>
                 </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
             </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+        </div>
+        <div id="node" class="carousel-item w-full">
+            <div class="grid grid-cols-3 gap-3 w-3/4">
+                <div class="flex justify-center my-5">
+                    <div class="w-24 h-24 overflow-hidden rounded-full">
+                        <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                    </div>
+                    <p class="text-2xl mt-8 ml-5">Joël Arana</p>
                 </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
             </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+        </div>
+        <div id="java" class="carousel-item w-full">
+            <div class="grid grid-cols-3 gap-3 w-3/4">
+                <div class="flex justify-center my-5">
+                    <div class="w-24 h-24 overflow-hidden rounded-full">
+                        <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                    </div>
+                    <p class="text-2xl mt-8 ml-5">Joël Arana</p>
                 </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
             </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+        </div>
+        <div id="dscience" class="carousel-item w-full">
+            <div class="grid grid-cols-3 gap-3 w-3/4">
+                <div class="flex justify-center my-5">
+                    <div class="w-24 h-24 overflow-hidden rounded-full">
+                        <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
+                    </div>
+                    <p class="text-2xl mt-8 ml-5">Joël Arana</p>
                 </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
             </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
-                </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
-            </div>
-            <div class="flex justify-center my-5">
-                <div class="w-24 h-24 overflow-hidden rounded-full">
-                    <img class="w-full h-full object-cover" src="{{ asset('img/collaborators/Joel_test.jpg') }}" />
-                </div>
-                <p class="text-2xl mt-8 ml-5">Joël Arana</p>
-            </div>
-            
         </div>
     </div>
 </section>
