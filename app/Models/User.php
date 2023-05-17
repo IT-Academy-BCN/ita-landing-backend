ab88e1a2c0c7916e1b074f19b0c9304f49b2fb35
Lauddar <lauragarciarod@gmail.com>
2023-05-17 10:18:48 +0200

    modify user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,10 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'dni',
         'password',
+        'status',
+        'role'
     ];
 
     /**
