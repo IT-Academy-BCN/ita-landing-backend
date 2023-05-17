01be94e7f8362ec4fa512a3e8336ad79d699442e
Lauddar <lauragarciarod@gmail.com>
2023-05-17 11:36:51 +0200

    add store feature

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -1,10 +1,59 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api;
 
+use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+     /**
+     * Create a new user.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        // Input validation
+        try {
+            $validatedData  = $request->validate([
+                'email' => 'required|string|email|max:255|unique:users',
+                'name' => 'string|max:255',
+                'dni' => 'required|string|unique:users',
+                'password' => 'required|string|min:8|confirmed',
+            ], [
+                'email.unique' => 'The email is already in use',
+                'dni.unique' => 'The DNI is already in use',
+                'password.confirmed' => 'The password confirmation does not match.',
+            ]);
+
+            // Create a new user.
+            $user = User::create([
+                'email' => $request->email,
+                'name' => $request->name,
+                'dni' => $request->dni,
+                'password' => Hash::make($request->password),
+                'status' => 'ACTIVE',
+                'role' => 'ADMIN',
+            ]);
+
+            // Response
+            return response()->json([
+                'result' => [
+                    'message' => 'User created succesfully.'
+                ],
+                'status' => true
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(
+                [
+                    'result' => ['message' => $e->getMessage()], 'status' => false
+                ],
+            );
+        }
+    }
 }
