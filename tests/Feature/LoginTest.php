43948ca2ffb0a7455edd94b39856ddb08c4d52af
Gabriela Maureira <gmaureirapalma@gmail.com>
2023-05-25 10:15:24 +0200

    Test user can not be logged with wrong dni

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -121,4 +121,37 @@ class LoginTest extends TestCase
         $this->assertGuest();
     }
 
+    /**
+     * A user cannot be logged in successfully with an incorrect DNI
+     */
+    public function test_a_user_cannot_be_logged_in_with_wrong_dni(): void
+    {
+        \Artisan::call('passport:install');
+
+        User::create([
+            'name' => 'Gabriela',
+            'email' => 'gaby@gmail.com',
+            'dni' => '39986946S',
+            'password' => bcrypt('password'),
+            'status' => 'ACTIVE',
+            'role' => 'ADMIN',
+        ]);
+
+        $response = $this->postJson(route('login'), [
+            'dni' => '39986987N',
+            'password' => 'password'
+        ]);
+        
+        $response->assertStatus(401);
+        $response->assertJson([
+            'result' => [
+                'message' => 'Invalid credentials'
+            ],
+            'status' => false
+        ]);
+
+        $this->assertGuest();
+    }
+
+
 }
