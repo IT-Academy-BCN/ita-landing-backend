dfc2c5180a08fe786756663542b54c916422efc8
Lauddar <lauragarciarod@gmail.com>
2023-05-30 21:16:03 +0200

    create faqs model and controller

---
+++ app/Http/Controllers/api/FaqsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FaqsController extends Controller
+{
+    public function index()
+    {
+        // To Do
+    }
+
+    public function show($id)
+    {
+        // To Do
+    }
+
+    public function create()
+    {
+        // To Do
+    }
+
+    public function store(Request $request)
+    {
+        // To Do
+    }
+
+    public function edit($id)
+    {
+        // To Do
+    }
+
+    public function update(Request $request, $id)
+    {
+        // To Do
+    }
+
+    public function destroy($id)
+    {
+        // To Do
+    }
+}
