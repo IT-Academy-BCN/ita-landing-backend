f738c3289715a4aa8f904cce3389e541936d97ee
Gabriela Maureira <gmaureirapalma@gmail.com>
2023-06-19 12:23:55 +0200

    Swagger installed

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        L5Swagger\L5SwaggerServiceProvider::class,
 
     ],
 
