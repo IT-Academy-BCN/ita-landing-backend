8cf249461b01bfdcb1410fac5cd9ce2783ecdf16
Gabriela Maureira <gmaureirapalma@gmail.com>
2023-07-12 22:53:44 +0200

    Automatic database backup working

--- config/database.php
+++ config/database.php
@@ -61,6 +61,11 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'dump' => [
+                'dump_binary_path' => '/Applications/XAMPP/bin', //for laragon "C:\laragon\bin\mysql\mysql-5.7.24-winx64\bin"
+                'use_single_transaction',
+                'timeout' => 60 * 5, // 5 minute timeout
+                ],
         ],
 
         'pgsql' => [
