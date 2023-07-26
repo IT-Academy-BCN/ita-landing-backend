d922bc8e2d401b386dae573a41cb7baa0281f673
levifvy <levifv.madrid@gmail.com>
2023-07-26 02:38:41 +0200

    setting TranslatableDatabasesvalue

--- app/Http/Middleware/TranslateDatabaseValues.php
+++ app/Http/Middleware/TranslateDatabaseValues.php
@@ -32,22 +32,3 @@ class TranslateDatabaseValues
         return $next($request);
     }
 }
-
-
-
-public function handle(Request $request, Closure $next): Response
-{
-    $locale = App::getLocale();
-
-    Builder::macro('translate', function ($column) use ($locale) {
-
-        return $this->leftJoin("{$column}_translations as {$column}_translation", function ($join) use ($column, $locale) {
-
-            $join->on("{$column}.id", '=', "{$column}_translation.{$column}_id")
-                    ->where("{$column}_translation.locale", '=', $locale);
-
-        })->addSelect("{$column}_translation.{$column}");
-    });
-
-    return $next($request);
-}
