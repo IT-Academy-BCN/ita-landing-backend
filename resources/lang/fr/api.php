55deca9ec2acdd2236959fa262bca8e6af6c69cf
Edvenan <e_vendrell@yahoo.com>
2023-10-04 16:54:33 +0200

    Clean up of files not required to meet the feature requirements

--- resources/lang/fr/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-return[
-    'name'          => 'Application multilingue',
-    'select_locale' => 'Sélectionnez votre langue',
-    'example'       => 'Exemple:',
-    'articles'      => 'article:',
-    'title'         => 'Titre',
-    'content'       => 'Contenu',
-];
