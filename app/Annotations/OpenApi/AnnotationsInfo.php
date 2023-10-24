ca94e3a2e2f0e5b8ce2442424bda75c2e67a990c
dustisme <androsrivas@outlook.com>
2023-10-24 09:44:58 +0200

    add comment in AnnotationsInfo.php

--- app/Annotations/OpenApi/AnnotationsInfo.php
+++ app/Annotations/OpenApi/AnnotationsInfo.php
@@ -9,7 +9,8 @@ namespace App\Annotations\OpenApi;
  *   description="Documentation needed for the API Rest for the landing page to IT Academy. Some useful links below:
  *   [ITA Landing Backend](https://github.com/IT-Academy-BCN/ita-landing-backend)
  *   [ITA Landing Frontend](https://github.com/IT-Academy-BCN/ita-landing-frontend)
- *   This API supports both Catalan and Spanish languages using the 'Accept-Language' header."
+ *   This API supports both Catalan and Spanish languages using the 'Accept-Language' header.
+ *   The default language is Catalan."
  * )
  *   @OA\Server(
  *     url="http://127.0.0.1:8000/api"
