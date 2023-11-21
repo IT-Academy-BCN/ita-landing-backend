cd445357ba2ae95f695acdf8fa3eda83a5383b85
dustisme <androsrivas@outlook.com>
2023-11-21 15:27:54 +0100

    Add annotations for excluding CollaboratorTest from massive testing.

--- tests/Feature/CollaboratorTest.php
+++ tests/Feature/CollaboratorTest.php
@@ -12,6 +12,9 @@ class CollaboratorTest extends TestCase
 {
     /**
      * A basic feature test example.
+     *
+     * @test
+     * @group CollaboratorTest
      */
     public function test_index_get_collaborators_landing(): void
     {
@@ -19,24 +22,39 @@ class CollaboratorTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_index_get_collaborators_ita_wiki(): void
     {
         $response = $this->get('/api/collaborators/wiki');
         $response->assertStatus(200);
     }
-
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_index_get_collaborators_challenges(): void
     {
         $response = $this->get('/api/collaborators/challenges');
         $response->assertStatus(200);
     }
     
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_inserting_on_index_a_area_that_dont_exist(): void
     {
         $response = $this->get('/api/collaborators/prueba');
         $response->assertStatus(404);
     }
 
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_collaborators_logic(): void
     {
 
@@ -47,6 +65,10 @@ class CollaboratorTest extends TestCase
         $this->assertIsArray($response);
     }
 
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_collaborator_landing_function(){
 
         $collaboratorsController = new CollaboratorsController();
@@ -54,6 +76,10 @@ class CollaboratorTest extends TestCase
         $this->assertIsArray($response);
     }
 
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_collaborator_ita_wiki_function(){
 
         $collaboratorsController = new CollaboratorsController();
@@ -61,6 +87,10 @@ class CollaboratorTest extends TestCase
         $this->assertIsArray($response);
     }
 
+    /**
+     * @test
+     * @group CollaboratorTest
+     */
     public function test_collaborator_challenges_function(){
 
         $collaboratorsController = new CollaboratorsController();
